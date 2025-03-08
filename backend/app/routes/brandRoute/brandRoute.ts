f84d66ebf136c7c88e5da51ae7212a9684ec28e4
ab70 <nurulabrar2369@gmail.com>
2025-03-08 11:26:45 +0600

    minor improvements done

--- backend/app/routes/brandRoute/brandRoute.ts
+++ backend/app/routes/brandRoute/brandRoute.ts
@@ -1,14 +1,15 @@
 import app from "../../app";
 import { z} from "@hono/zod-openapi";
-import { checkAdmin } from "../../middlewares/authMiddleware";
+import { checkAdmin, checkVendor } from "../../middlewares/authMiddleware";
 import { contentType, Delete, Get, Patch, Post } from "../../utils/swagger/methods"
 import brandControllers from "../../controllers/brandControllers/brandController";
 import { BrandPostSchema } from "../../models/Brand";
+//***** */ Replace check vendor to checkAdmin ******
 // Create brand
-app.openapi(Post({ path: "/brand/craete", tags: ["Brand"], middleware:[checkAdmin], schema: BrandPostSchema, type: contentType.multipartFormData }), brandControllers().createBrand);
+app.openapi(Post({ path: "/brand/craete", tags: ["Brand"], middleware:[checkVendor], schema: BrandPostSchema, type: contentType.multipartFormData }), brandControllers().createBrand);
 // Get brand
 app.openapi(Get({ path: "/brand/get", tags: ["Brand"] }), brandControllers().getAllBrands);
 // Edit brand
-app.openapi(Patch({ path: "/brand/edit", query: z.object({ id: z.string() }),  tags: ["Brand"], middleware:[checkAdmin], schema: BrandPostSchema }), brandControllers().updateBrand);
+app.openapi(Patch({ path: "/brand/edit", query: z.object({ id: z.string() }),  tags: ["Brand"], middleware:[checkVendor], schema: BrandPostSchema }), brandControllers().updateBrand);
 // Delete brand
-app.openapi(Delete({ path: "/brand/delete", query: z.object({ id: z.string() }), tags: ["Brand"], middleware:[checkAdmin] }), brandControllers().deleteBrand);
+app.openapi(Delete({ path: "/brand/delete", query: z.object({ id: z.string() }), tags: ["Brand"], middleware:[checkVendor] }), brandControllers().deleteBrand);
--- backend/app/routes/categoryRoute/categoryRoute.ts
+++ backend/app/routes/categoryRoute/categoryRoute.ts
@@ -1,14 +1,15 @@
 import app from "../../app";
 import { z } from "@hono/zod-openapi";
 import categoryControllers from "../../controllers/categoryControllers/categoryControllers";
-import { checkAdmin, checkUser } from "../../middlewares/authMiddleware";
+import { checkAdmin, checkUser, checkVendor } from "../../middlewares/authMiddleware";
 import { CategoryPostSchema } from "../../models/Category";
 import { Delete, Get, Patch, Post } from "../../utils/swagger/methods"
+//***** */ Replace checkVendor to checkAdmin ******
 // Create category
-app.openapi(Post({ path: "/category/craete", tags: ["Category"], middleware: [checkAdmin], schema: CategoryPostSchema }), categoryControllers().createCategory);
+app.openapi(Post({ path: "/category/craete", tags: ["Category"], middleware: [checkVendor], schema: CategoryPostSchema }), categoryControllers().createCategory);
 // Get category
 app.openapi(Get({ path: "/category/get", tags: ["Category"] }), categoryControllers().getCategories);
 // Edit category
-app.openapi(Patch({ path: "/category/edit", query: z.object({ id: z.string() }), tags: ["Category"], middleware: [checkAdmin], schema: CategoryPostSchema }), categoryControllers().updateCategory);
+app.openapi(Patch({ path: "/category/edit", query: z.object({ id: z.string() }), tags: ["Category"], middleware: [checkVendor], schema: CategoryPostSchema }), categoryControllers().updateCategory);
 // Delete category
-app.openapi(Delete({ path: "/category/delete", query: z.object({ id: z.string() }), tags: ["Category"], middleware: [checkAdmin] }), categoryControllers().deleteCategory);
+app.openapi(Delete({ path: "/category/delete", query: z.object({ id: z.string() }), tags: ["Category"], middleware: [checkVendor] }), categoryControllers().deleteCategory);
