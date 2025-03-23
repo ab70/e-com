4c8d0f576eeba378d1be7659ceaca259e4987170
ab70 <nurulabrar2369@gmail.com>
2025-03-23 14:33:04 +0600

    fix brnad and category middleware

--- backend/app/routes/brandRoute/brandRoute.ts
+++ backend/app/routes/brandRoute/brandRoute.ts
@@ -6,10 +6,10 @@ import brandControllers from "../../controllers/brandControllers/brandController
 import { BrandPostSchema } from "../../models/Brand";
 //***** */ Replace check vendor to checkAdmin ******
 // Create brand
-app.openapi(Post({ path: "/brand/craete", tags: ["Brand"], middleware:[checkVendor], schema: BrandPostSchema }), brandControllers().createBrand);
+app.openapi(Post({ path: "/brand/craete", tags: ["Brand"], middleware:[checkAdmin], schema: BrandPostSchema }), brandControllers().createBrand);
 // Get brand
 app.openapi(Get({ path: "/brand/get", tags: ["Brand"] }), brandControllers().getAllBrands);
 // Edit brand
-app.openapi(Patch({ path: "/brand/edit", query: z.object({ id: z.string() }),  tags: ["Brand"], middleware:[checkVendor], schema: BrandPostSchema }), brandControllers().updateBrand);
+app.openapi(Patch({ path: "/brand/edit", query: z.object({ id: z.string() }),  tags: ["Brand"], middleware:[checkAdmin], schema: BrandPostSchema }), brandControllers().updateBrand);
 // Delete brand
-app.openapi(Delete({ path: "/brand/delete", query: z.object({ id: z.string() }), tags: ["Brand"], middleware:[checkVendor] }), brandControllers().deleteBrand);
+app.openapi(Delete({ path: "/brand/delete", query: z.object({ id: z.string() }), tags: ["Brand"], middleware:[checkAdmin] }), brandControllers().deleteBrand);
