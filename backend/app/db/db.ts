31aceb0ad3af90af45e2f3a929b863a7ae1d4603
ab70 <nurulabrar2369@gmail.com>
2025-03-23 14:33:41 +0600

    db separation

--- backend/app/db/db.ts
+++ backend/app/db/db.ts
@@ -3,7 +3,7 @@ import { adminSignupUser_func } from '../controllers/authControllers/functions/a
 import { UserRole } from '../models/User';
 
 // mongoose.set('strictQuery', true); // set it to false if needs to query the database by any field
-const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://abrar:1234@cluster0.cht5w.mongodb.net/ecom?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://abrar:1234@cluster0.cht5w.mongodb.net/ecom1?retryWrites=true&w=majority";
 const connection = mongoose.connect(MONGO_URL).then(async (response) => {
     console.log("MongoDB connected successfully-->");
     const userObj = {
